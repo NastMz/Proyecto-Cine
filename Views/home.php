96a6c4991ea65a16bad79f6da482f16391c5147e
Kevin Martinez <ksmartinez2307@gmail.com>
2022-04-01 20:02:13 -0500

    Vista pagina principal

--- Views/home.php
+++ Views/home.php
@@ -6,16 +6,153 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $data['page_tag']; ?></title>
+    <link rel="stylesheet" href="Assets/css/style.css">
 </head>
 <body>
-<?php
-echo passGenerator(16);
-echo '<br/>';
-echo tokenGenerator();
-?>
-<section id="<?php echo $data['page_id']; ?>">
-    <h1><?php echo $data['page_title']; ?></h1>
-    <p><?php echo $data['page_content']; ?></p>
-</section>
+<header>
+    <div class="logo">
+        <img src="Assets/images/logo_cineco.svg" alt="cinecolombia logo" class="logo-image"/>
+    </div>
+    <div class="user">
+        <img src="Assets/images/user.png" alt="user icon" class="user-icon"/>
+        <div class="user-name"><span>Usuario</span></div>
+    </div>
+</header>
+<main>
+    <div class="movies-container">
+        <div class="movie-card">
+            <div class="movie-image">
+                <img src="Assets/images/Bienvenido.png" alt="cinecolombia logo" class="logo-image"/>
+            </div>
+            <div class="movie-info">
+                <h2>Titulo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis facilis magnam minus
+                    molestias nostrum officia optio perferendis similique temporibus. Enim est hic nesciunt officia
+                    perferendis quas saepe sint voluptates?</p>
+            </div>
+            <div class="button-buy">
+                <input type="submit" value="Comprar">
+            </div>
+        </div>
+
+        <div class="movie-card">
+            <div class="movie-image">
+                <img src="Assets/images/Bienvenido.png" alt="cinecolombia logo" class="logo-image"/>
+            </div>
+            <div class="movie-info">
+                <h2>Titulo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis facilis magnam minus
+                    molestias nostrum officia optio perferendis similique temporibus. Enim est hic nesciunt officia
+                    perferendis quas saepe sint voluptates?</p>
+            </div>
+            <div class="button-buy">
+                <input type="submit" value="Comprar">
+            </div>
+        </div>
+
+        <div class="movie-card">
+            <div class="movie-image">
+                <img src="Assets/images/Bienvenido.png" alt="cinecolombia logo" class="logo-image"/>
+            </div>
+            <div class="movie-info">
+                <h2>Titulo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis facilis magnam minus
+                    molestias nostrum officia optio perferendis similique temporibus. Enim est hic nesciunt officia
+                    perferendis quas saepe sint voluptates?</p>
+            </div>
+            <div class="button-buy">
+                <input type="submit" value="Comprar">
+            </div>
+        </div>
+
+        <div class="movie-card">
+            <div class="movie-image">
+                <img src="Assets/images/Bienvenido.png" alt="cinecolombia logo" class="logo-image"/>
+            </div>
+            <div class="movie-info">
+                <h2>Titulo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis facilis magnam minus
+                    molestias nostrum officia optio perferendis similique temporibus. Enim est hic nesciunt officia
+                    perferendis quas saepe sint voluptates?</p>
+            </div>
+            <div class="button-buy">
+                <input type="submit" value="Comprar">
+            </div>
+        </div>
+
+        <div class="movie-card">
+            <div class="movie-image">
+                <img src="Assets/images/Bienvenido.png" alt="cinecolombia logo" class="logo-image"/>
+            </div>
+            <div class="movie-info">
+                <h2>Titulo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque corporis facilis magnam minus
+                    molestias nostrum officia optio perferendis similique temporibus. Enim est hic nesciunt officia
+                    perferendis quas saepe sint voluptates?</p>
+            </div>
+            <div class="button-buy">
+                <input type="submit" value="Comprar">
+            </div>
+        </div>
+    </div>
+</main>
+<footer>
+    <div class="container">
+        <div class="columns-footer">
+            <div class="logo-blue">
+                <img src="Assets/images/logo_cineco_blue.svg" alt="cinecolombia logo" class="logo-blue-image"/>
+            </div>
+            <div class="info">
+                <ul class="footer_nav">
+                    <li>
+                        <a href="#">Información Legal</a>
+                    </li>
+                    <li>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <a href="#">Acerca de Cineco</a>
+                    </li>
+                    <li>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <a href="#">Contáctanos PQRS</a>
+                    </li>
+                    <li>
+                        <span>|</span>
+                    </li>
+                    <li>
+                        <a href="#">Preguntas Frecuentes</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="social">
+                <p>Síguenos en redes sociales</p>
+                <ul class="footer_social">
+                    <li>
+                        <a href="https://www.facebook.com/cinecolombiaoficial">
+                            <img src="Assets/images/logo-facebook.svg" alt="facebook icon" class="icon"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/Cine_Colombia">
+                            <img src="Assets/images/logo-twitter.svg" alt="user icon" class="icon"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/cinecolombia">
+                            <img src="Assets/images/logo-instagram.svg" alt="user icon" class="icon"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer_legal">
+            <p>&copy; 2021 Cine Colombia - Proyecto Bases de Datos</p>
+            <p>Kevin Santiago Martinez - Joshep Mateo Granda</p>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
