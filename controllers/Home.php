637fd4599423791b878eabb06832c9a64645b0f6
Kevin Martinez <ksmartinez2307@gmail.com>
2022-04-01 20:02:32 -0500

    Vista pagina principal

--- controllers/Home.php
+++ controllers/Home.php
@@ -19,37 +19,4 @@ class Home extends Controllers
         $this->views->getView($this, "home", $data);
     }
 
-    public function save()
-    {
-        $data = array("1007590327", "Kevin Santiago", "Martinez Martinez", 1);
-        $request = $this->model->saveUser($data);
-        print_r($request);
-    }
-
-    public function find($id)
-    {
-        $request = $this->model->findUserById($id);
-        print_r($request);
-    }
-
-    public function update()
-    {
-        $id = "1007590327";
-        $data = array("Kevin Santiago", "Martinez Martinez", 1, "ksmartinez@gmail.com", "123456");
-        $request = $this->model->updateUser($id, $data);
-        print_r($request);
-    }
-
-    public function findAll()
-    {
-        $request = $this->model->findUsers();
-        print_r($request);
-    }
-
-    public function delete($id)
-    {
-        $request = $this->model->deleteUser($id);
-        print_r($request);
-    }
-
 }
